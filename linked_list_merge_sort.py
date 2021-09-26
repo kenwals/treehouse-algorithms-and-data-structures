6039a8549c7357bc2e48b976c9151d209377d029
kenwals <kenwals@gmail.com>
2021-09-26 13:55:28 +0100

    link list merge sort

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -2,28 +2,30 @@ from linked_list import LinkedList
 
 def merge_sort(linked_list):
     """Sorts a linked list in ascending order
-    recursively divide the linked list into sublists containing a single node
-    repeatedly merge the sublists to produce sorted sublists until one remains
+    - recursively divide the linked list into sublists containing a single node
+    - repeatedly merge the sublists to produce sorted sublists until one remains
     
-    Returnsa sorted linked list
+    Returns a sorted linked list
 
+    Runs in O(kn log n)
     Args:
         linked_list ([type]): [description]
     """
-    if linked_list.size()==1 :
+    if linked_list.size()==1 :  # list is size 1
         return linked_list
-    elif linked_list is None:
+    elif linked_list is None:  # list is empty
         return linked_list
     
     left_half, right_half = split(linked_list)
     left = merge_sort(left_half)
     right = merge_sort(right_half)
     
-    return(left,right)
+    return merge(left,right)
 
 def split(linked_list):
     """
     Divide the unsorted list at midpoint into sublists
+    Takes O(k log n) time
 
     Args:
         linked_list ([type]): [description]
@@ -34,4 +36,88 @@ def split(linked_list):
         
         return left_half, right_half
     else:
+        size = linked_list.size()
+        mid = size//2
         
+        mid_node = linked_list.node_at_index(mid-1)
+        
+        left_half = linked_list
+        right_half = LinkedList()
+        right_half.head = mid_node.next_node
+        mid_node.next_node = None 
+        
+        return left_half, right_half
+
+def merge(left, right):
+    """
+    Merges two linked lists, sorting by data in the nodes
+    Returns a new, merged list 
+    Runs in O(n) Linear time
+
+    Args:
+        left ([type]): [description]
+        right ([type]): [description]
+    """
+    # create a new linked list that contains nodes from 
+    # merging left and right
+    merged = LinkedList()
+    
+    # Add a fake head that is discarded later
+    merged.add(0)
+    
+    # set current to the head of the linked list
+    current = merged.head
+    
+    #Obtain head nodes for left and right linked lists
+    left_head = left.head
+    right_head = right.head 
+    
+    # Iterate over left and right until we reach the tail node
+    # of either
+    while left_head or right_head:
+        # If the head node of left is None , we're past the tail
+        # Add the node from the right to merged linked list
+        if left_head is None:
+            current.next_node = right_head
+            # Call next on right set loop condition to False
+            right_head = right_head.next_node
+        # if the head node of right is None, we're past the tail
+        # Add the tail node from left to merged linked list
+        elif right_head is None:
+            current.next_node = left_head
+            # Call next on left set loop condition to False
+            left_head = left_head.next_node
+        else:
+            # Not at either tail node
+            # Obtain node data to perform comparision operations
+            left_data = left_head.data
+            right_data = right_head.data
+            # if data on left is less than right, set current to left node
+            if left_data < right_data:
+                current.next_node = left_head
+                # move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right , set current to right node
+            else:
+                current.next_node = right_head
+                # move right head to next node
+                right_head = right_head.next_node
+        # Move current to next node
+        current = current.next_node
+    
+    # Discard fake head and set first merged node as head
+    head = merged.head.next_node
+    merged.head = head
+    
+    return merged
+
+l = LinkedList()
+l.add(10)
+l.add(2)
+l.add(44)
+l.add(15)
+l.add(200)
+
+print(l)
+sorted_linked_list = merge_sort(l)
+print(sorted_linked_list)
